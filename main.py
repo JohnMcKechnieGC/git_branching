a2e4e007c42294dcaf89563b5c2256af6003102d
jmmckechnie <jmmckechnie@gmail.com>
2025-05-29 16:29:07 +0100

    Add a testable function called add

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 """
-Version: 1.0.3
+Version: 1.0.4
 """
-print('Hello, Feature!')
+
+def add(a: int, b: int) -> int:
+    """Returns the sum of two integers."""
+    return a + b
+
+
+if __name__ == "__main__":
+    print('Hello, testable function!')
